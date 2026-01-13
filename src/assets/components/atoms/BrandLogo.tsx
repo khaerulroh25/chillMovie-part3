9924d5df56ba0109351f0318692c931eea404a82
khaerul25 <khaerulroh@gmail.com>
2026-01-13 15:47:06 +0700

    update brandlogo

--- src/assets/components/atoms/BrandLogo.tsx
+++ src/assets/components/atoms/BrandLogo.tsx
@@ -3,12 +3,13 @@ import logo from "../../img/icons/logo.png";
 export default function BrandLogo() {
   return (
     <div className="flex items-center gap-2">
-      <img src={logo} alt="Chill Logo" className="w-[50px] h-[44px]" />
+      <img src={logo} alt="Chill Logo" className="w-6 h-6 md:w-10 md:h-10" />
 
       <span
         className="
           font-brand
-          text-[50px]
+          text-[20px]
+          md:text-[48px]
           text-white
           leading-[55px]
           h-[44px]
