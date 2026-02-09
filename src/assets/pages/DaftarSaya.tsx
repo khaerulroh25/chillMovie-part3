2a147ba6512c2031c6229bd7291806b4f8ac7b8a
khaerul25 <khaerulroh@gmail.com>
2026-02-09 17:52:44 +0700

    update

--- src/assets/pages/DaftarSaya.tsx
+++ src/assets/pages/DaftarSaya.tsx
@@ -38,7 +38,7 @@ export default function DaftarSaya() {
   };
 
   return (
-    <div className="md:w-[1440px] md:h-[1237px] bg-black text-white font-sans">
+    <div className="md:w-[1440px] min-h-screen bg-black text-white font-sans">
       <Navbar />
 
       <div className="px-4 md:px-[80px] py-10">
